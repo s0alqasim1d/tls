441651da242666ee2947eb6877b5d2719cc4bcdb
Steven Penny <srpen6+4cq2@gmail.com>
2023-05-07 19:29:02 -0500

    deleted:    u_parrots.go

--- u_common.go
+++ u_common.go
@@ -4,7 +4,99 @@
 
 package tls
 
-import "fmt"
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"strconv"
+)
+
+// ApplyPreset should only be used in conjunction with HelloCustom to apply custom specs.
+// Also used internally.
+func (uconn *UConn) ApplyPreset(p *ClientHelloSpec) error {
+	hello := uconn.HandshakeState.Hello
+	session := uconn.HandshakeState.Session
+
+	if hello.Vers == 0 {
+		hello.Vers = VersionTLS12
+	}
+	switch len(hello.Random) {
+	case 0:
+		hello.Random = make([]byte, 32)
+		_, err := io.ReadFull(uconn.config.rand(), hello.Random)
+		if err != nil {
+			return errors.New("tls: short read from Rand: " + err.Error())
+		}
+	case 32:
+	// carry on
+	default:
+		return errors.New("ClientHello expected length: 32 bytes. Got: " +
+			strconv.Itoa(len(hello.Random)) + " bytes")
+	}
+	if len(hello.CipherSuites) == 0 {
+		hello.CipherSuites = defaultCipherSuites()
+	}
+	if len(hello.CompressionMethods) == 0 {
+		hello.CompressionMethods = []uint8{compressionNone}
+	}
+
+	// Currently, GREASE is assumed to come from BoringSSL
+	grease_bytes := make([]byte, 2*ssl_grease_last_index)
+	grease_extensions_seen := 0
+	_, err := io.ReadFull(uconn.config.rand(), grease_bytes)
+	if err != nil {
+		return errors.New("tls: short read from Rand: " + err.Error())
+	}
+	for i := range uconn.greaseSeed {
+		uconn.greaseSeed[i] = binary.LittleEndian.Uint16(grease_bytes[2*i : 2*i+2])
+	}
+	if uconn.greaseSeed[ssl_grease_extension1] == uconn.greaseSeed[ssl_grease_extension2] {
+		uconn.greaseSeed[ssl_grease_extension2] ^= 0x1010
+	}
+
+	hello.CipherSuites = p.CipherSuites
+	for i := range hello.CipherSuites {
+		if hello.CipherSuites[i] == GREASE_PLACEHOLDER {
+			hello.CipherSuites[i] = GetBoringGREASEValue(uconn.greaseSeed, ssl_grease_cipher)
+		}
+	}
+	uconn.GetSessionID = p.GetSessionID
+
+	uconn.Extensions = p.Extensions
+
+	for _, e := range uconn.Extensions {
+		switch ext := e.(type) {
+		case *SNIExtension:
+			if ext.ServerName == "" {
+				ext.ServerName = uconn.config.ServerName
+			}
+		case *FakeGREASEExtension:
+			switch grease_extensions_seen {
+			case 0:
+				ext.Value = GetBoringGREASEValue(uconn.greaseSeed, ssl_grease_extension1)
+			case 1:
+				ext.Value = GetBoringGREASEValue(uconn.greaseSeed, ssl_grease_extension2)
+				ext.Body = []byte{0}
+			default:
+				return errors.New("at most 2 grease extensions are supported")
+			}
+			grease_extensions_seen += 1
+		case *SessionTicketExtension:
+			err := uconn.SetSessionState(session)
+			if err != nil {
+				return err
+			}
+		case *SupportedCurvesExtension:
+			for i := range ext.Curves {
+				if ext.Curves[i] == GREASE_PLACEHOLDER {
+					ext.Curves[i] = CurveID(GetBoringGREASEValue(uconn.greaseSeed, ssl_grease_group))
+				}
+			}
+		}
+	}
+	return nil
+}
 
 // Naming convention:
 // Unsupported things are prefixed with "Fake"
