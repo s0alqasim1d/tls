d49246896c1ad7cf0a7845001483f286379cae43
Steven Penny <srpen6+4cq2@gmail.com>
2023-05-09 05:15:44 -0500

    new file:   blog/tls.go

--- tls_test.go
+++ tls_test.go
@@ -1,25 +1,25 @@
-package main
+package tls
 
 import (
-	"2a.pages.dev/tls"
 	"bufio"
 	"fmt"
 	"net"
 	"net/http"
 	"net/url"
 	"strings"
+	"testing"
 )
 
-func main() {
-	conf := tls.Config{ServerName: "android.googleapis.com"}
+func Test_TLS(t *testing.T) {
+	conf := Config{ServerName: "android.googleapis.com"}
 	dial_conn, err := net.Dial("tcp", "android.googleapis.com:443")
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
-	tls_conn := tls.UClient(dial_conn, &conf)
+	tls_conn := UClient(dial_conn, &conf)
 	defer tls_conn.Close()
-	if err := tls_conn.ApplyPreset(&tls.Android_API_26); err != nil {
-		panic(err)
+	if err := tls_conn.ApplyPreset(&Android_API_26); err != nil {
+		t.Fatal(err)
 	}
 	body := url.Values{
 		"Email":              {email},
@@ -32,21 +32,21 @@ func main() {
 		strings.NewReader(body),
 	)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Proto = "HTTP/1.1"
 	req.ProtoMajor = 1
 	req.ProtoMinor = 1
 	if err := req.Write(tls_conn); err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	res, err := http.ReadResponse(bufio.NewReader(tls_conn), req)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	if err := res.Body.Close(); err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	fmt.Println(res.Status)
 }
